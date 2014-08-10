c2b44035845719eaf1925352c7b1009400a7b934
Kristoffer Dalby <kradalby@kradalby.no>
2014-08-10 18:37:11 +0200

    initial commit

---
+++ gso.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+from SourceQuery import SourceQuery as q
+
+app = Flask(__name__)
+
+SERVERS = (
+    ('z.fap.no', 27015),
+    ('z.fap.no', 27016),
+    ('z.fap.no', 27017),
+)
+
+@app.route('/')
+def index():
+    servers = []
+    for server in SERVERS:
+        servers.append(q(server[0], server[1]).info())
+    return render_template('index.html', servers=servers)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
